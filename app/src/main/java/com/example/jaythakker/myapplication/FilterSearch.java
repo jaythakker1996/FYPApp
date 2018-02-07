c1edd8aed2135c86697c10d8cff0c58a97eaf4b9
Kaushal Suvarna <31623058+kaushal1120@users.noreply.github.com>
2018-02-07 15:20:59 +0530

    Updated MainActivity and Register activity ! Merge branch 'master' of https://github.com/jaythakker1996/FYPApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/jaythakker/myapplication/FilterSearch.java
    #	app/src/main/java/com/example/jaythakker/myapplication/LocationHelper.java
    #	app/src/main/java/com/example/jaythakker/myapplication/PermissionUtils.java
    #	app/src/main/java/com/example/jaythakker/myapplication/Restaurant.java
    #	app/src/main/res/layout/activity_filter_search.xml
    #	app/src/main/res/values/array.xml

--- app/src/main/java/com/example/jaythakker/myapplication/FilterSearch.java
+++ app/src/main/java/com/example/jaythakker/myapplication/FilterSearch.java
@@ -1,15 +1,19 @@
 package com.example.jaythakker.myapplication;
-
+//(add restaurant markers, enable location searching)
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DialogFragment;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Location;
 import android.location.LocationManager;
+import android.media.Image;
 import android.media.audiofx.BassBoost;
 import android.os.Build;
 import android.os.Parcelable;
@@ -19,14 +23,21 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -47,38 +58,81 @@ import org.json.JSONObject;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import android.content.res.Resources;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class FilterSearch extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, ActivityCompat.OnRequestPermissionsResultCallback, com.google.android.gms.location.LocationListener {
+        GoogleApiClient.OnConnectionFailedListener, ActivityCompat.OnRequestPermissionsResultCallback, com.google.android.gms.location.LocationListener, OnMapReadyCallback{
 
     private static final String TAG = "Logging App Activity";
     private Location mLastLocation;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    private GoogleMap mMap;
+    private CameraPosition mCameraPosition;
+
+    private ProgressDialog mProgress;
 
     double latitude;
     double longitude;
+    String address;
 
     private LocationHelper locationHelper;
+
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
 
     @RequiresApi(api = Build.VERSION_CODES.M)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        // Retrieve location and camera position from saved instance state.
+        if (savedInstanceState != null) {
+            mLastLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
         setContentView(R.layout.activity_filter_search);
 
         final EditText noOfPersonsEntered = (EditText) findViewById(R.id.noOfPersons);
         final EditText budgetEntered = (EditText) findViewById(R.id.budget);
-        final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
         final Spinner cuisineSelected = (Spinner) findViewById(R.id.cuisine);
         final Button search = (Button) findViewById(R.id.search);
-        final ImageButton getLoc = (ImageButton) findViewById(R.id.location);
+        final TextView dispLoc = (TextView) findViewById(R.id.location);
+        final ImageButton getLoc = (ImageButton) findViewById(R.id.imageButton);
         final RequestQueue queue = VolleyQueue.getInstance(this.getApplicationContext()).getRequestQueue();
+        final Context context = this;
+        final ImageButton history = (ImageButton) findViewById(R.id.history);
+        final ImageButton recommendations = (ImageButton) findViewById(R.id.recommendations);
 
         locationHelper = new LocationHelper(this);
         locationHelper.checkpermission();
+
+        mProgress = new ProgressDialog(this);
+        mProgress.setTitle("Processing...");
+        mProgress.setMessage("Please wait...");
+        mProgress.setCancelable(false);
+        mProgress.setIndeterminate(true);
+
+        final SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,24 +146,28 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
                 catch(NumberFormatException ex){
                     ex.printStackTrace();
                 }
-                final String time = timePicker.getCurrentHour() + ":" + timePicker.getCurrentMinute();
+
                 String cuisine = cuisineSelected.getSelectedItem().toString();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
-                builder.setMessage(noOfPersons + " " + budget + " " + time + " " + cuisine)
+                /*AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
+                builder.setMessage(noOfPersons + " " + budget + " " + " " + cuisine)
                         .create()
                         .show();
+                */
 
-                String url ="http://192.168.1.101:8080/search/";
+                String url ="http://192.168.43.113:8080/search/";
                 Response.Listener list=new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
+                        mProgress.dismiss();
+
+                        /*AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
                         builder.setMessage(response.toString())
                                 .create()
-                                .show();
+                                .show();*/
+
                         if(response != null) {
                             try {
 
@@ -117,10 +175,10 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
                                 ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
 
                                 for (int i = 0; i < jsonRests.length(); i++) {
-                                    AlertDialog.Builder builder1 = new AlertDialog.Builder(FilterSearch.this);
+                                    /*AlertDialog.Builder builder1 = new AlertDialog.Builder(FilterSearch.this);
                                     builder1.setMessage(jsonRests.getJSONObject(i).getString("name"))
                                             .create()
-                                            .show();
+                                            .show();*/
                                     restaurants.add(i, new Restaurant(jsonRests.getJSONObject(i).getString("restId"),
                                             jsonRests.getJSONObject(i).getString("name"),
                                             jsonRests.getJSONObject(i).getString("area"),
@@ -139,11 +197,10 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
                         }
                         else{
                             AlertDialog.Builder builder2 = new AlertDialog.Builder(FilterSearch.this);
-                            builder2.setMessage("Can't connect to the server at this moment! Please try again.")
+                            builder2.setMessage("Can't connect to the server at this moment! Please try again later.")
                                     .create()
                                     .show();
                         }
-
                     }
                 };
 
@@ -151,18 +208,19 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        mProgress.dismiss();
                         error.printStackTrace();
                         // TODO Auto-generated method stub
                         AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
-                        builder.setMessage("Error : "+error)
+                        builder.setMessage("Can't connect to the server at this moment! Please try again later.")
                                 .create()
                                 .show();
                     }
                 };
 
                 try {
+
                     JSONObject jsonObj = new JSONObject();
-                    jsonObj.put("time", time);
                     jsonObj.put("noOfPersons", noOfPersons);
                     jsonObj.put("budget", budget);
                     jsonObj.put("cuisine", cuisine);
@@ -176,27 +234,67 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
                 JSONRequest req = new JSONRequest(Request.Method.GET,url,null,list,err);
                 // Access the RequestQueue through your singleton class.
                 queue.add(req);
+                mProgress.show();
             }
         });
 
         getLoc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 mLastLocation = locationHelper.getLocation();
                 if (mLastLocation != null) {
                     latitude = mLastLocation.getLatitude();
                     longitude = mLastLocation.getLongitude();
-                    AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
-                    builder.setMessage(latitude + " : " + longitude)
-                            .create()
-                            .show();
-                    getAddress();
+                    LatLng myLaLn = new LatLng(latitude, longitude);
+
+                    CameraPosition camPos = new CameraPosition.Builder().target(myLaLn)
+                            .zoom(15)
+                            .bearing(45)
+                            .build();
+
+                    CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
+
+                    if(mMap != null) {
+                        mMap.animateCamera(camUpd3);
+                        mMap.clear();
+                        MarkerOptions markerOpts = new MarkerOptions().position(myLaLn).title("my Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+                        mMap.addMarker(markerOpts);
+                    }
+
+                    address = getAddress();
+                    if(address != null)
+                        dispLoc.setText(address.substring(0, 28) + "...");
+                    else
+                        showToast("Something went wrong ! Try again later.");
 
                 } else {
-
-                    showToast("Couldn't get the location. Make sure location is enabled on the device");
+                    showToast("Couldn't get the location. Make sure location is enabled on the device.");
                 }
+            }
+        });
+
+        dispLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(FilterSearch.this);
+                builder1.setMessage(address)
+                        .create()
+                        .show();
+
+            }
+        });
+
+        recommendations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
@@ -206,44 +304,46 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
 
     }
 
+    /**
+     * Saves the state of the map when the activity is paused.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (mMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, mLastLocation);
+            super.onSaveInstanceState(outState);
+        }
+    }
+
     protected void onStart(){
         super.onStart();
         if(!locationHelper.isPermissionGranted()){
-            AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
-            builder.setMessage("Permission not granted")
-                    .create()
-                    .show();
+            showToast("Location permissions not granted !");
         }
         else{
-            AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
-            builder.setMessage("Permission granted")
-                    .create()
-                    .show();
+            showToast("Location permissions granted !");
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        showToast("Stopping...");
     }
 
-    public void getAddress()
+    public String getAddress()
     {
         Address locationAddress;
-
         locationAddress = locationHelper.getAddress(latitude,longitude);
 
         if(locationAddress!=null)
         {
-
             String address = locationAddress.getAddressLine(0);
             String address1 = locationAddress.getAddressLine(1);
             String city = locationAddress.getLocality();
             String state = locationAddress.getAdminArea();
             String country = locationAddress.getCountryName();
             String postalCode = locationAddress.getPostalCode();
-
 
             String currentLocation;
 
@@ -252,36 +352,27 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
                 currentLocation=address;
 
                 if (!TextUtils.isEmpty(address1))
-                    currentLocation+="\n"+address1;
-
+                    currentLocation+=", "+address1;
                 if (!TextUtils.isEmpty(city))
                 {
-                    currentLocation+="\n"+city;
-
+                    currentLocation+=", "+city;
                     if (!TextUtils.isEmpty(postalCode))
                         currentLocation+=" - "+postalCode;
                 }
                 else
                 {
                     if (!TextUtils.isEmpty(postalCode))
-                        currentLocation+="\n"+postalCode;
+                        currentLocation+=", "+postalCode;
                 }
-
                 if (!TextUtils.isEmpty(state))
-                    currentLocation+="\n"+state;
-
+                    currentLocation+=", "+state;
                 if (!TextUtils.isEmpty(country))
-                    currentLocation+="\n"+country;
+                    currentLocation+=", "+country;
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(FilterSearch.this);
-                builder.setMessage(currentLocation)
-                        .create()
-                        .show();
+                return currentLocation;
             }
-
         }
-        else
-            showToast("Something went wrong");
+        return null;
     }
 
     @Override
@@ -289,19 +380,17 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
         locationHelper.onActivityResult(requestCode,resultCode,data);
     }
 
-    /**
-     * Google api callback methods
-     */
+    //Google api callback methods
 
     @Override
     public void onConnectionFailed(ConnectionResult result) {
-        showToast("Connection Failed!");
+        showToast("Connection to Google API Failed!");
     }
 
     @Override
     public void onConnected(Bundle arg0) {
         // Once connected with google api, get the location
-        showToast("Connected...");
+        showToast("Connected to Google API");
         mLocationRequest = locationHelper.getLocationRequest();
         mGoogleApiClient = locationHelper.getGoogleApiCLient();
 
@@ -311,8 +400,7 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionSuspended(int arg0) {
         locationHelper.connectApiClient();
-        showToast("Suspended");
-
+        showToast("Connection to Google API Suspended");
     }
 
     // Permission check functions
@@ -321,7 +409,6 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
                                            @NonNull int[] grantResults) {
         // redirects to utils
         locationHelper.onRequestPermissionsResult(requestCode,permissions,grantResults);
-
     }
 
     public void showToast(String message)
@@ -329,10 +416,29 @@ public class FilterSearch extends AppCompatActivity implements GoogleApiClient.C
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 
-
     @Override
     public void onLocationChanged(Location location) {
         mLastLocation = location;
-        showToast("Connection Update...");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+        // Position the map's camera near Sydney, Australia.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
     }
 }
